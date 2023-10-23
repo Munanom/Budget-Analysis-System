769727df4915f8aad9105c8f50dd896bf6cfbc7a
Muna Numan <munanuman@Munas-MacBook-Pro.local>
2023-10-24 00:08:08 +0300

    code to filter out the data and put all necessary on a dict

--- dataset_creation/trial.py
+++ dataset_creation/trial.py
@@ -1,24 +1,43 @@
 import pandas as pd
 # Replace 'your_file.xlsx' with the actual file path of your Excel file
-df = pd.read_excel('/Users/muna/Documents/iMessage-Data.xlsx', sheet_name='iMessages')
+df = pd.read_excel('/Users/munanuman/Documents/sch/iMessage-Data.xlsx', sheet_name='iMessages')
 
 column_names = df.columns
-print(column_names)
+# print(column_names)
 
 
 # Assuming the 'User_ID' column contains 'MPesa'
 messages_from_mpesa = df[df['User_ID'] == 'MPESA']
-print(messages_from_mpesa)
-
-# messages_from_mpesa [81].split()[2][3:]
-for i in messages_from_mpesa:
-    try:
-        paid=float(i.split()[2][3:])
-    except ValueError:
-        pass
-    else:
-        print(paid)
 
 
+# # messages_from_mpesa [81].split()[2][3:]
+# for i,row in messages_from_mpesa:
+#     try:
+#         paid=float(i.split()[2][3:])
+#     except ValueError:
+#         pass
+#     else:
+#         print(paid)
 
+
+import re
+
+info = {}
+dates_list = list(messages_from_mpesa['Date'])  # Convert dates to a list
+
+for message, date in zip(messages_from_mpesa['Message'], dates_list):
+    
+    m = message.split()[2:]
+        
+    if m[1] == "sent":
+        user = re.search(r"sent to ([\w\s]+?)\s\d", message).group(1)
+        payment = float(m[0][3:].replace(',', ''))
+        info[user] = {'payment': payment, 'date': date}
+       
+    elif m[1] == "paid":
+        user = re.search(r"paid to ([\w\s]+)\.", " ".join(m[:9])).group(1)
+        payment = float(m[0][3:].replace(',', ''))
+        info[user] = {'payment': payment, 'date': date}
+        
+print(info)
 
